c0deffc33d4a91538dc4f2f18fc4e6ee29348137
Paul Dawkins <dawkins.paul@hotmail.com>
2017-01-23 14:18:14 -0500

    Create 01 Bonfire Meet Bonfire.js

---
+++ Basic Algorithm Scripting/01 Bonfire Meet Bonfire.js
@@ -0,0 +1,26 @@
+/* Bonfire: Meet Bonfire
+Difficulty: 0/5
+
+Click the button below for further instructions.
+
+Your goal is to fix the failing test.
+
+First, run all the tests by clicking "Run code" or by pressing Control + Enter
+
+The failing test is in red. Fix the code so that all tests pass. Then you can move on to the next
+Bonfire.
+
+Make this function return true no matter what.
+
+Coded by Paul Dawkins
+dawkins.paul@hotmail.com
+https://www.freecodecamp.com/pd19
+*/
+
+function meetBonfire(argument) {
+  console.log("you can read this function's argument in the developer tools", argument);
+
+  return true;
+}
+
+meetBonfire("You can do this!");
